807737f6f2c8ceb8d6fd55fae5a9e07ef41543e0
Karol Wiśniewski <k.wisniewski.417@studms.ug.edu.pl>
2022-10-23 20:14:57 +0200

    Done feature_4

--- app/Konto.py
+++ app/Konto.py
@@ -1,6 +1,6 @@
 class Konto:
-    def __init__(self, imie, nazwisko, pesel):
+    def __init__(self, imie, nazwisko, pesel, kod = None):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.saldo = 0
+        self.saldo = 0 if kod == None else 50 if kod.startswith("PROM_") and len(kod) == 8 else 0
         self.pesel = pesel if len(pesel) == 11 else "Niepoprawny pesel!"
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -15,4 +15,12 @@ class TestCreateBankAccount(unittest.TestCase):
         drugie_konto = Konto("Mariusz", "Januszewski", "8709088927")
         self.assertEqual(drugie_konto.pesel, "Niepoprawny pesel!")
 
+    def test_poprawny_kod_promocyjny(self):
+        trzecie_konto = Konto("Monika", "Kociołek", "92090889272", "PROM_-V6")
+        self.assertEqual(trzecie_konto.saldo, 50, "Saldo się nie zgadza!")
+
+    def test_niepoprawny_kod_promocyjny(self):
+        trzecie_konto = Konto("Monika", "Kociołek", "92090889272", "PROM_-V69")
+        self.assertEqual(trzecie_konto.saldo, 0, "Saldo się nie zgadza!")
+
     #tutaj proszę dodawać nowe testy
