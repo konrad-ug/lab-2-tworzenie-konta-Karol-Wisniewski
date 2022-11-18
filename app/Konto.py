42132e46c5fc13ad72369f2fe879df18941dc3b7
Karol-Wisniewski <karol-wisniewski2001@wp.pl>
2022-11-18 14:47:36 +0100

    Repaired feature_12

--- app/Konto.py
+++ app/Konto.py
@@ -4,15 +4,20 @@ class Konto:
         self.nazwisko = nazwisko
         self.saldo = 50 if kod != None and kod.startswith("PROM_") and len(kod) == 8 and oblicz_rok_urodzenia_z_peselu(pesel) >= 1960 else 0
         self.pesel = pesel if len(pesel) == 11 else "Niepoprawny pesel!"
+        self.historia = []
 
     def zaksieguj_przelew_wychodzacy(self, kwota):
         self.saldo = self.saldo - kwota if kwota <= self.saldo and kwota > 0 else self.saldo
+        self.historia.append(-kwota)
 
     def zaksieguj_przelew_wychodzacy_ekspresowy(self, kwota):
         self.saldo = self.saldo - kwota - 1 if kwota <= self.saldo and kwota > 0 else self.saldo
+        self.historia.append(-kwota)
+        self.historia.append(-1)
 
     def zaksieguj_przelew_przychodzacy(self, kwota):
         self.saldo = self.saldo + kwota if kwota > 0 else self.saldo
+        self.historia.append(kwota)
 
     def zaciagnij_kredyt(self, kwota):
         ostatnie_3_transakcje_czy_dodatnie = all(i > 0 for i in self.historia[-3:])
@@ -24,15 +29,17 @@ class Konto:
             return False
 
 
-
 class KontoFirmowe(Konto):
     def __init__(self, nazwa_firmy, NIP):
         self.nazwa_firmy = nazwa_firmy
         self.NIP = NIP if len(NIP) == 10 else "Niepoprawny NIP!"
         self.saldo = 0
+        self.historia = []
 
     def zaksieguj_przelew_wychodzacy_ekspresowy(self, kwota):
         self.saldo = self.saldo - kwota - 5 if kwota <= self.saldo and kwota > 0 else self.saldo
+        self.historia.append(-kwota)
+        self.historia.append(-5)
 
 
 
